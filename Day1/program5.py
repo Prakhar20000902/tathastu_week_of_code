aa7775cfd45fc74cd1a37c8100f7c4cae839337c
Prakhar Singh <66130201+Prakhar20000902@users.noreply.github.com>
2020-05-30 21:04:07 +0530

    Create program5.py

---
+++ Day1/program5.py
@@ -0,0 +1,24 @@
+p1 =int (input("runs scored by first player"))
+p2 = int (input("runs scored by second player"))
+p3 = int (input("runs scored by third player"))
+srp1 = p1 * (100/60)
+srp2 = p2 * (100/60)
+srp3 = p3 * (100/60)
+print("strike rate of players are:")
+print("player 1 = " , srp1)
+print("player 2 = " , srp2)
+print("player 3 = " , srp3)
+rsp1 = p1 * 2
+rsp2 = p2 * 2
+rsp3 = p3 * 2
+print("runs scored after playing 60 more balls are:")
+print("player 1 = :" , rsp1)
+print("player 2 = :" , rsp2)
+print("player 3 = :" , rsp3)
+msixp1 = p1 // 6
+msixp2 = p2 // 6
+msixp3 = p3 // 6
+print("maximum number of sixes by each player are:")
+print("player 1 =" , msixp1)
+print("player 2 =" , msixp2)
+print("player 3 =" , msixp3)
