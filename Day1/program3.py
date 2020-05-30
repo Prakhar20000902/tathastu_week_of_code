63dcfde87b657a78336315b5968204fdeb38c1e5
Prakhar Singh <66130201+Prakhar20000902@users.noreply.github.com>
2020-05-30 20:29:59 +0530

    Create program3.py

---
+++ Day1/program3.py
@@ -0,0 +1,7 @@
+n1=float(input("Enter first number"))
+n2=float(input("Enter second number"))
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+print ("After swapping the numbers\n" "Value of n1=",n1)
+print("Value of n2=",n2)
